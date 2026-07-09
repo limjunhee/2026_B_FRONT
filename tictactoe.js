8c350cc3f24ee918f01ec451a6d351b9ba2ab07b
limjunhee <ski725400@gmail.com>
2026-07-09 15:13:02 +0900

    Day4_5

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,10 @@
 /*
 3x3 보드를 임의의 1차원 배열로 표현한다(이때, 사용할 인덱스는 0 ~ 8)
 
+[보드 번호]
+1   2   3
+4   5   6
+7   8   9
 플레이어 1, 플레이어 2가 번갈아가며 원하는 인덱스에 prompt를 통해 넣는다. -> 이 단계를 총 9번 반복
 
 총 9번 반복 후 보드가 모두 채워져 있을 것이다.(배열이 모두 채워져 있음)
@@ -11,25 +15,34 @@
 */
 
 let arr = [" "," "," "," "," "," "," "," "," "]
+
 let choice1 = Number(prompt("1. 틱택토 보드 번호 입력(1~9)"))
 arr[choice1 - 1] = "X"
 // console.log(choice1)
 // console.log(arr[0])
 // console.log(arr)
+
 let choice2 = Number(prompt("2. 틱택토 보드 번호 입력(1~9)"))
 arr[choice2 - 1] = "O"
+
 let choice3 = Number(prompt("3. 틱택토 보드 번호 입력(1~9)"))
 arr[choice3 - 1] = "X"
+
 let choice4 = Number(prompt("1. 틱택토 보드 번호 입력(1~9)"))
 arr[choice4 - 1] = "O"
+
 let choice5 = Number(prompt("5. 틱택토 보드 번호 입력(1~9)"))
 arr[choice5 - 1] = "X"
+
 let choice6 = Number(prompt("6. 틱택토 보드 번호 입력(1~9)"))
 arr[choice6 - 1] = "O"
+
 let choice7 = Number(prompt("7. 틱택토 보드 번호 입력(1~9)"))
 arr[choice7 - 1] = "X"
+
 let choice8 = Number(prompt("8. 틱택토 보드 번호 입력(1~9)"))
 arr[choice8 - 1] = "O"
+
 let choice9 = Number(prompt("1. 틱택토 보드 번호 입력(1~9)"))
 arr[choice9 - 1] = "X"
 
